81aa0a7f5a9fc70a0a9f0a5f66ee3e6a3b164d52
Raghav Bharadwaj <raghavbharadwaj001@gmail.com>
2026-07-11 21:22:59 +0530

    Implement Redis-backed job queue using Redis Lists

---
+++ src/controllers/jobs.redis.controller.js
@@ -0,0 +1,45 @@
+const redisClient = require("../redis/redis");
+
+function delay(ms){
+    return new Promise((resolve)=>{
+        setTimeout(resolve , ms);
+    });
+}
+
+async function createJob(req, res) {
+    const job = {
+        id: Date.now(),
+        ...req.body,
+    };
+
+    await redisClient.rpush("jobs", JSON.stringify(job));
+
+    res.status(201).json({
+        message: "Job created successfully",
+        job,
+    });
+}
+
+async function getJobs(req, res) {
+    const jobs = await redisClient.lrange("jobs", 0, -1);
+
+    const parsedJobs = jobs.map((job) => JSON.parse(job));
+
+    res.status(200).json(parsedJobs);
+}
+
+async function createJobSync(req, res) {
+
+    await delay(5000);
+
+    res.status(200).json({
+        message: "Synchronous job completed"
+    });
+
+}
+
+module.exports = {
+    createJob,
+    getJobs,
+    createJobSync,
+};
